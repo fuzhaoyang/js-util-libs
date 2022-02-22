8a95d2851e9a63a2535f54e28dc848cac48d69f8
fuzhaoyang <932647051@qq.com>
2022-02-22 16:45:21 +0800

    fix：浏览器内核

--- src/checkBrowser/index.js
+++ src/checkBrowser/index.js
@@ -2,12 +2,30 @@
  * 判断是浏览器内核
  */
 exports.checkBrowser = () => {
-    const u = navigator.userAgent;
-    const obj = {
-        trident: u.indexOf("Trident") > -1, //IE内核
-        presto: u.indexOf("Presto") > -1, //opera内核
-        webKit: u.indexOf("AppleWebKit") > -1, //苹果、谷歌内核
-        gecko: u.indexOf("Gecko") > -1 && u.indexOf("KHTML") == -1, //火狐内核
-    };
-    return Object.keys(obj)[Object.values(obj).indexOf(true)];
+    let t = navigator.userAgent.toLowerCase();
+    return 0 <= t.indexOf("msie") ? { //ie < 11
+        type: "IE",
+        version: Number(t.match(/msie ([\d]+)/)[1])
+    } : !!t.match(/trident\/.+?rv:(([\d.]+))/) ? { // ie 11
+        type: "IE",
+        version: 11
+    } : 0 <= t.indexOf("edge") ? {
+        type: "Edge",
+        version: Number(t.match(/edge\/([\d]+)/)[1])
+    } : 0 <= t.indexOf("firefox") ? {
+        type: "Firefox",
+        version: Number(t.match(/firefox\/([\d]+)/)[1])
+    } : 0 <= t.indexOf("chrome") ? {
+        type: "Chrome",
+        version: Number(t.match(/chrome\/([\d]+)/)[1])
+    } : 0 <= t.indexOf("opera") ? {
+        type: "Opera",
+        version: Number(t.match(/opera.([\d]+)/)[1])
+    } : 0 <= t.indexOf("Safari") ? {
+        type: "Safari",
+        version: Number(t.match(/version\/([\d]+)/)[1])
+    } : {
+        type: t,
+        version: -1
+    }
 };
